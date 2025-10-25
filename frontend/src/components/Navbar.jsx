3499ec6505b26ea57fcc08b5b67a9a3057fcc610
immanuelonly01 <immanuelonly01@gmail.com>
2025-10-25 08:28:20 +0530

    connect to frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,12 +8,11 @@ export default function Navbar() {
       <div className="container">
         <Link className="navbar-brand neon-text" to="/">Person Search AI</Link>
         
-        {/* 1. THE TOGGLE BUTTON */}
         <button 
             className="navbar-toggler" 
             type="button" 
-            data-bs-toggle="collapse" // Controls the collapse action
-            data-bs-target="#navbarNav" // Targets the div below
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav" 
             aria-controls="navbarNav"
             aria-expanded="false"
             aria-label="Toggle navigation"
@@ -21,11 +20,9 @@ export default function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         
-        {/* 2. THE COLLAPSIBLE MENU */}
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             
-            {/* Home is in the correct position */}
             <li className="nav-item"><Link className="nav-link" to="/"><FaHome className="me-1" /> Home</Link></li>
             
             <li className="nav-item"><Link className="nav-link" to="/upload"><FaUpload className="me-1" /> File Upload</Link></li>
